fac0acd750615d5f6149ae05ba1364cb247b2129
lostm1nd <kliomba@abv.bg>
2014-02-09 12:14:11 +0200

    Outline for screen manager
    Added ScreenManager.cs and BaseScreen.cs

---
+++ FyodorDostoevskyRPG/FyodorDostoevskyRPG/FyodorDostoevskyRPG/ScreenManager/BaseScreen.cs
@@ -0,0 +1,34 @@
+﻿namespace FyodorDostoevskyRPG.ScreenManager
+{
+    using Microsoft.Xna.Framework;
+
+    public abstract class BaseScreen
+    {
+        // Fields
+        private string name;
+        private Vector2 position;
+        private ScreenState state = ScreenState.Active;
+
+        // Methods -> trqbva da se override ot vseki otdlen ekran
+        protected virtual void HandleInput()
+        {
+        }
+
+        protected virtual void Load()
+        {
+        }
+
+        protected virtual void Unload()
+        {
+            state = ScreenState.ShutDown;
+        }
+
+        protected virtual void Update()
+        {
+        }
+
+        protected virtual void Draw()
+        {
+        }
+    }
+}
