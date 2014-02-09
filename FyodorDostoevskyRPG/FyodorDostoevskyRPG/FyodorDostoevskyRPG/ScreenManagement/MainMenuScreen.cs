ac7188b1808ca229258f17cf5fabb81986031c85
lostm1nd <kliomba@abv.bg>
2014-02-09 17:33:49 +0200

    Updated ScreenManger + added 2 screens
    TitleScreen and MainMenu screen
    Press "Space" to toggle between screens

---
+++ FyodorDostoevskyRPG/FyodorDostoevskyRPG/FyodorDostoevskyRPG/ScreenManagement/MainMenuScreen.cs
@@ -0,0 +1,39 @@
+﻿namespace FyodorDostoevskyRPG.ScreenManagement
+{
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
+    using Microsoft.Xna.Framework.Input;
+
+    public class MainMenuScreen : BaseScreen
+    {
+        // Fields
+        private KeyboardState currentKey, previousKey;
+
+        // Methods
+        public override void LoadContent(ContentManager content)
+        {
+            base.LoadContent(content);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            this.previousKey = this.currentKey;
+            this.currentKey = Keyboard.GetState();
+
+            if (this.previousKey.IsKeyDown(Keys.Space) && this.currentKey.IsKeyUp(Keys.Space))
+            {
+                ScreenManager.Instance.LoadScreen(new TitleScreen());
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
